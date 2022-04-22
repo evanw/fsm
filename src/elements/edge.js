f765039b7434b8cf8308f7b67e921a1c5ad0c6c4
thebigbadme <38137021+thebigbadme@users.noreply.github.com>
2022-04-22 16:30:35 +0000

    Update edge.js

--- src/elements/edge.js
+++ src/elements/edge.js
@@ -51,7 +51,7 @@ Edge.prototype.getEndPointsAndCircle = function() {
     var circle = circleFromThreePoints(this.vertexA.x, this.vertexA.y, this.vertexB.x, this.vertexB.y, anchor.x, anchor.y);
     var isReversed = (this.perpendicularPart > 0);
     var reverseScale = isReversed ? 1 : -1;
-    
+
     var radiusA = this.vertexA.radius;
     var radiusB = this.vertexB.radius;
     var startAngle = Math.atan2(this.vertexA.y - circle.y, this.vertexA.x - circle.x) - reverseScale * radiusA / circle.radius;
@@ -159,104 +159,4 @@ Edge.prototype.containsPoint = function(x, y) {
         return (percent > 0 && percent < 1 && Math.abs(distance) < hitTargetPadding);
     }
     return false;
-};
-
-		};
-	}
-	var anchor = this.getAnchorPoint();
-	var circle = circleFromThreePoints(this.nodeA.x, this.nodeA.y, this.nodeB.x, this.nodeB.y, anchor.x, anchor.y);
-	var isReversed = (this.perpendicularPart > 0);
-	var reverseScale = isReversed ? 1 : -1;
-	var startAngle = Math.atan2(this.nodeA.y - circle.y, this.nodeA.x - circle.x) - reverseScale * nodeRadius / circle.radius;
-	var endAngle = Math.atan2(this.nodeB.y - circle.y, this.nodeB.x - circle.x) + reverseScale * nodeRadius / circle.radius;
-	var startX = circle.x + circle.radius * Math.cos(startAngle);
-	var startY = circle.y + circle.radius * Math.sin(startAngle);
-	var endX = circle.x + circle.radius * Math.cos(endAngle);
-	var endY = circle.y + circle.radius * Math.sin(endAngle);
-	return {
-		'hasCircle': true,
-		'startX': startX,
-		'startY': startY,
-		'endX': endX,
-		'endY': endY,
-		'startAngle': startAngle,
-		'endAngle': endAngle,
-		'circleX': circle.x,
-		'circleY': circle.y,
-		'circleRadius': circle.radius,
-		'reverseScale': reverseScale,
-		'isReversed': isReversed,
-	};
-};
-
-Link.prototype.draw = function(c) {
-	var stuff = this.getEndPointsAndCircle();
-	// draw arc
-	c.beginPath();
-	if(stuff.hasCircle) {
-		c.arc(stuff.circleX, stuff.circleY, stuff.circleRadius, stuff.startAngle, stuff.endAngle, stuff.isReversed);
-	} else {
-		c.moveTo(stuff.startX, stuff.startY);
-		c.lineTo(stuff.endX, stuff.endY);
-	}
-	c.stroke();
-	// draw the head of the arrow
-	if(stuff.hasCircle) {
-		drawArrow(c, stuff.endX, stuff.endY, stuff.endAngle - stuff.reverseScale * (Math.PI / 2));
-	} else {
-		drawArrow(c, stuff.endX, stuff.endY, Math.atan2(stuff.endY - stuff.startY, stuff.endX - stuff.startX));
-	}
-	// draw the text
-	if(stuff.hasCircle) {
-		var startAngle = stuff.startAngle;
-		var endAngle = stuff.endAngle;
-		if(endAngle < startAngle) {
-			endAngle += Math.PI * 2;
-		}
-		var textAngle = (startAngle + endAngle) / 2 + stuff.isReversed * Math.PI;
-		var textX = stuff.circleX + stuff.circleRadius * Math.cos(textAngle);
-		var textY = stuff.circleY + stuff.circleRadius * Math.sin(textAngle);
-		drawText(c, this.text, textX, textY, textAngle, selectedObject == this);
-	} else {
-		var textX = (stuff.startX + stuff.endX) / 2;
-		var textY = (stuff.startY + stuff.endY) / 2;
-		var textAngle = Math.atan2(stuff.endX - stuff.startX, stuff.startY - stuff.endY);
-		drawText(c, this.text, textX, textY, textAngle + this.lineAngleAdjust, selectedObject == this);
-	}
-};
-
-Link.prototype.containsPoint = function(x, y) {
-	var stuff = this.getEndPointsAndCircle();
-	if(stuff.hasCircle) {
-		var dx = x - stuff.circleX;
-		var dy = y - stuff.circleY;
-		var distance = Math.sqrt(dx*dx + dy*dy) - stuff.circleRadius;
-		if(Math.abs(distance) < hitTargetPadding) {
-			var angle = Math.atan2(dy, dx);
-			var startAngle = stuff.startAngle;
-			var endAngle = stuff.endAngle;
-			if(stuff.isReversed) {
-				var temp = startAngle;
-				startAngle = endAngle;
-				endAngle = temp;
-			}
-			if(endAngle < startAngle) {
-				endAngle += Math.PI * 2;
-			}
-			if(angle < startAngle) {
-				angle += Math.PI * 2;
-			} else if(angle > endAngle) {
-				angle -= Math.PI * 2;
-			}
-			return (angle > startAngle && angle < endAngle);
-		}
-	} else {
-		var dx = stuff.endX - stuff.startX;
-		var dy = stuff.endY - stuff.startY;
-		var length = Math.sqrt(dx*dx + dy*dy);
-		var percent = (dx * (x - stuff.startX) + dy * (y - stuff.startY)) / (length * length);
-		var distance = (dx * (y - stuff.startY) - dy * (x - stuff.startX)) / length;
-		return (percent > 0 && percent < 1 && Math.abs(distance) < hitTargetPadding);
-	}
-	return false;
 };
