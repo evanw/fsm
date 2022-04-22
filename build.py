458d3ee5c3aa7d7e17873d8b8026a0c64757660d
thebigbadme <38137021+thebigbadme@users.noreply.github.com>
2022-04-22 16:41:53 +0000

    Update build.py

--- build.py
+++ build.py
@@ -3,29 +3,35 @@
 import os, time, sys
 
 def sources():
-	path = './src/'
-	return [os.path.join(base, f) for base, folders, files in os.walk(path) for f in files if f.endswith('.js')]
+    path = './src/'
+    return [os.path.join(base, f) for base, folders, files in os.walk(path) for f in files if f.endswith('.js')]
 
 def build():
-	path = './www/fsm.js'
-	data = '\n'.join(open(file, 'r').read() for file in sources())
-	with open(path, 'w') as f:
-		f.write(data)
-	print 'built %s (%u bytes)' % (path, len(data))
+    path = './www/graph.js'
+    try:
+        os.remove(path)
+        pass
+    except Exception as e:
+        pass#if there's an exception, it's because file doesn't exist already
+
+    data = '\n'.join(open(file, 'r').read() for file in sources())
+    with open(path, 'w') as f:
+        f.write(data)
+    print ('built %s (%u bytes)' % (path, len(data)))
 
 def stat():
-	return [os.stat(file).st_mtime for file in sources()]
+    return [os.stat(file).st_mtime for file in sources()]
 
 def monitor():
-	a = stat()
-	while True:
-		time.sleep(0.5)
-		b = stat()
-		if a != b:
-			a = b
-			build()
+    a = stat()
+    while True:
+        time.sleep(0.5)
+        b = stat()
+        if a != b:
+            a = b
+            build()
 
 if __name__ == '__main__':
-	build()
-	if '--watch' in sys.argv:
-		monitor()
+    build()
+    if '--watch' in sys.argv:
+        monitor()
